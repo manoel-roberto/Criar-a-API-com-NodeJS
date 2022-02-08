b1a2c36fc56792f44131c4506fa0fdc6ff17811b
Manoel Roberto <manoelrobertoms@gmail.com>
2022-02-08 12:29:49 -0300

    Repositório com o código atualizado

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,17 @@
 import { request, response, Router } from "express";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
-import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const CategoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  CategoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  })
-
-
-  categories.push(category);
-
-  return response.status(201).json({ category });
+  return response.status(201).send();
 })
 
 export { categoriesRoutes };
