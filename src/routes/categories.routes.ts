e7538f32a0296a32189957d18161de1c3ee3303e
Manoel Roberto <manoelrobertoms@gmail.com>
2022-02-08 15:12:19 -0300

    Correção a variável está com o mesmo nome da importação

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,12 +4,12 @@ import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 
-const CategoriesRepository = new CategoriesRepository();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  CategoriesRepository.create({ name, description });
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 })
