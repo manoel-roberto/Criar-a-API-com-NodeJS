2f6d492f5fdd1754c118a52ac022621f8886df0e
Manoel Roberto <manoelrobertoms@gmail.com>
2022-02-07 22:27:52 -0300

    Debugando aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express, { request, response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ menssage: "Hello World" })
+})
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 })
 
 
